1b64cd163fefc1bec726682989ca4b85e8959d5b
ghostyboyy97 <106448956+ghostyboyy97@users.noreply.github.com>
2025-09-30 08:38:45 -0400

    fix (choice lock): update handling of ability to GetBattlerAbility()

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2969,7 +2969,7 @@ void StealTargetItem(u8 battlerStealer, u8 battlerItem)
     BtlController_EmitSetMonData(battlerItem, BUFFER_A, REQUEST_HELDITEM_BATTLE, 0, sizeof(gBattleMons[gBattlerTarget].item), &gBattleMons[battlerItem].item);  // remove target item
     MarkBattlerForControllerExec(battlerItem);
 
-    if (gBattleMons[gBattlerTarget].ability != ABILITY_GORILLA_TACTICS && gBattleMons[gBattlerTarget].ability != ABILITY_SAGE_POWER)
+    if (GetBattlerAbility(gBattlerTarget) != ABILITY_GORILLA_TACTICS && GetBattlerAbility(gBattlerTarget) != ABILITY_SAGE_POWER)
         gBattleStruct->choicedMove[gBattlerTarget] = MOVE_NONE;
 
     TrySaveExchangedItem(battlerItem, gLastUsedItem);
@@ -5706,7 +5706,7 @@ static bool32 TryKnockOffBattleScript(u32 battlerDef)
 
             gLastUsedItem = gBattleMons[battlerDef].item;
             gBattleMons[battlerDef].item = 0;
-            if (gBattleMons[battlerDef].ability != ABILITY_GORILLA_TACTICS && gBattleMons[battlerDef].ability != ABILITY_SAGE_POWER)
+            if (GetBattlerAbility(gBattlerTarget) != ABILITY_GORILLA_TACTICS && GetBattlerAbility(gBattlerTarget) != ABILITY_SAGE_POWER)
                 gBattleStruct->choicedMove[battlerDef] = MOVE_NONE;
             CheckSetUnburden(battlerDef);
 
@@ -14499,9 +14499,9 @@ static void Cmd_tryswapitems(void)
             BtlController_EmitSetMonData(gBattlerTarget, BUFFER_A, REQUEST_HELDITEM_BATTLE, 0, sizeof(gBattleMons[gBattlerTarget].item), &gBattleMons[gBattlerTarget].item);
             MarkBattlerForControllerExec(gBattlerTarget);
 
-            if (gBattleMons[gBattlerTarget].ability != ABILITY_GORILLA_TACTICS && gBattleMons[gBattlerTarget].ability != ABILITY_SAGE_POWER)
+            if (GetBattlerAbility(gBattlerTarget) != ABILITY_GORILLA_TACTICS && GetBattlerAbility(gBattlerTarget) != ABILITY_SAGE_POWER)
                 gBattleStruct->choicedMove[gBattlerTarget] = MOVE_NONE;
-            if (gBattleMons[gBattlerAttacker].ability != ABILITY_GORILLA_TACTICS && gBattleMons[gBattlerAttacker].ability != ABILITY_SAGE_POWER)
+            if (GetBattlerAbility(gBattlerAttacker) != ABILITY_GORILLA_TACTICS && GetBattlerAbility(gBattlerAttacker) != ABILITY_SAGE_POWER)
                 gBattleStruct->choicedMove[gBattlerAttacker] = MOVE_NONE;
 
             gBattlescriptCurrInstr = cmd->nextInstr;
